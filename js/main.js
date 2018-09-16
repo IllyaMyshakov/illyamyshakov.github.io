de090e8ecaf67376779c172a01cf18b9d8366dbe
IllyaMyshakov <35241874+IllyaMyshakov@users.noreply.github.com>
2018-09-16 12:53:32 -0400

    Version 2.0

--- js/main.js
+++ js/main.js
@@ -12,17 +12,3 @@ const buttons = Array.from(document.querySelectorAll('.button')) // instanciate 
 const count = buttons.length // number of buttons
 const increase = Math.PI * 2 / buttons.length // finding central angle by dividing 2*pi / number of buttons
 const radius = 175 // radius of your circle
-
-buttons.forEach((button, i) => { // function that initializes position of buttons 
-  button.style.top = Math.sin(-Math.PI / 2 + i * increase) * radius + 'px'
-  button.style.left = Math.cos(-Math.PI / 2 + i * increase) * radius + 'px'
-  button.addEventListener('click', move)
-})
-
-function move(e) { // Move button function
-  const n = buttons.indexOf(e.target)
-  buttons.forEach((button, i) => { // function that moves buttons to new position
-    button.style.top = Math.sin(-Math.PI / 2 + (i - n % count) * increase) * radius + 'px'
-    button.style.left = Math.cos(-Math.PI / 2 + (i - n % count) * increase) * radius + 'px'
-  })
-}
