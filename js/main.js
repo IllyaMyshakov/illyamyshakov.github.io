478cf52a67a8afd4e9155544ba8775409ca7381f
IllyaMyshakov <35241874+IllyaMyshakov@users.noreply.github.com>
2018-09-16 13:45:43 -0400

    menu fix

--- js/main.js
+++ js/main.js
@@ -12,3 +12,8 @@ const buttons = Array.from(document.querySelectorAll('.button')) // instanciate 
 const count = buttons.length // number of buttons
 const increase = Math.PI * 2 / buttons.length // finding central angle by dividing 2*pi / number of buttons
 const radius = 175 // radius of your circle
+
+buttons.forEach((button, i) => { // function that initializes position of buttons 
+    button.style.top = Math.sin(-Math.PI / 2 + i * increase) * radius + 'px'
+    button.style.left = Math.cos(-Math.PI / 2 + i * increase) * radius + 'px'
+})
